a00541e276dc6cc36e774969f7cb2dc78afcc1c9
grandchace <66575104+grandchace@users.noreply.github.com>
2024-05-08 20:00:00 -0700

    Update hex-reroll-source.js

--- hex-reroll-source.js
+++ hex-reroll-source.js
@@ -1,5 +1,7 @@
+document.getElementsByClassName('refresh')[0].click();
 
-function sumDigitsFromString(str) {  var nums = [];
+function sumDigitsFromString(str) {
+  var nums = [];
   var sum = 0;
 
   for (var i = 0; i < str.length; i++) {
@@ -16,7 +18,7 @@ function sumDigitsFromString(str) {  var nums = [];
 
 function hexLoop() {
     eval("var hex = rgbToHex" + document.getElementsByClassName("rand-color")[0].style.backgroundColor.slice(3) + ";");
-if (sumDigitsFromString(hex) != hexGoal) {
+if (sumDigitsFromString(hex) != 0) {
     document.getElementsByClassName('refresh')[0].click();
 setTimeout(function() {
     hexLoop();
@@ -24,12 +26,8 @@ setTimeout(function() {
 } else {alert(hex)}
 }
 hexGoal = window.prompt("How much the numbers sum up to in the hex code. Enter nothing or press cancel to stop trying.")
-console.log(hexGoal);
 if (hexGoal == null || hexGoal === '') {
-    hexLoop = function(){};
-} else {
-    document.getElementsByClassName('refresh')[0].click();
-    hexLoop()
+hexLoop = function(){};
 }
 function componentToHex(c) {
   var hex = c.toString(16);
@@ -41,3 +39,4 @@ function rgbToHex(r, g, b) {
 }
 
 
+hexLoop()
